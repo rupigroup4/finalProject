462195d1303af4140fa562a38b2b14992ed737b7
rupigroup4 <59657569+rupigroup4@users.noreply.github.com>
2020-04-11 14:07:04 +0300

    commit37

--- final_project_WEB/sql/Request.sql
+++ final_project_WEB/sql/Request.sql
@@ -1,16 +1,16 @@
 create table Request_igroup4 (
-    _requestID int identity(1,1) not null,
-	_date_time varchar(10) not null,
-    _numTickets int not null,
-    _status varchar(20) not null,
+    requestID int identity(1,1) not null,
+	date_time varchar(10) not null,
+    numTickets int not null,
+    status_ varchar(20) not null,
 	pdfFile varchar(max),
-    _TripID  int not null,
-	primary key (_requestID),
-	foreign key (_TripID) REFERENCES Trip_igroup4(_id)
+    TripID  int not null,
+	primary key (requestID),
+	foreign key (TripID) REFERENCES Trip_igroup4(_id)
 );
 
 drop table Request_igroup4;
 
 select * from Request_igroup4;
 
-SELECT COUNT(requestID) as count_ FROM Request_igroup4 where _status='new'
+SELECT COUNT(requestID) as count_ FROM Request_igroup4 where status_='new'
