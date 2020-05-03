9224e5f4464c87e28cb2f8f90679e0043eb91eea
rupigroup4 <59657569+rupigroup4@users.noreply.github.com>
2020-05-03 16:26:10 +0300

    Update SQLPromotedAttractionQuery.sql

--- final_project_WEB/sql/SQLPromotedAttractionQuery.sql
+++ final_project_WEB/sql/SQLPromotedAttractionQuery.sql
@@ -1,17 +1,11 @@
+drop table PromotedAttraction_igroup4
 create table PromotedAttraction_igroup4 (
     attracionID varchar (30) not null,
 	ordersQuantity int DEFAULT 0,
     promoted bit DEFAULT 0,
+	tripProfile varchar(10)DEFAULT '0',
+	cityName varchar(255) not null,
 	primary key (attracionID)
 );
-
-UPDATE PromotedAttraction_igroup4 SET promoted = 0 WHERE attracionID = 'Kalverstraat';
-
-select * from PromotedAttraction_igroup4
-
-select COUNT(attracionID) from PromotedAttraction_igroup4 where attracionID='kkk'
-
-
-drop table PromotedAttraction_igroup4
-
-select COUNT(attracionID) as count_ from PromotedAttraction_igroup4 where attracionID='Kalverstraat'
+	select * from PromotedAttraction_igroup4 
+	select tripProfile from PromotedAttraction_igroup4 where attracionID='ggg'
