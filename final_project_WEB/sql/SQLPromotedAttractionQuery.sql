f0589a969f3069f0fcda4b157c8c314873e29c96
rupigroup4 <59657569+rupigroup4@users.noreply.github.com>
2020-05-06 22:45:25 +0300

    commit 80

--- final_project_WEB/sql/SQLPromotedAttractionQuery.sql
+++ final_project_WEB/sql/SQLPromotedAttractionQuery.sql
@@ -1,13 +1,21 @@
 drop table PromotedAttraction_igroup4
 create table PromotedAttraction_igroup4 (
+	agent_ID int not null,
     attracionID varchar (30) not null,
-	ordersQuantity int DEFAULT 0,
-    promoted bit DEFAULT 0,
-	tripProfile varchar(10)DEFAULT '0',
+    rate int,	
 	cityName varchar(255) not null,
+	ordersQuantity int DEFAULT 0,
+	_1 int DEFAULT '0',
+	_2 int DEFAULT '0',
+	_3 int DEFAULT '0',
+	_4 int DEFAULT '0',
+	_5 int DEFAULT '0',
 	primary key (attracionID)
 );
 	select * from PromotedAttraction_igroup4 
-	select tripProfile from PromotedAttraction_igroup4 where attracionID='ggg'
+	UPDATE PromotedAttraction_igroup4 SET _3= 1 WHERE attracionID = 'Kalverstraat'
 
-UPDATE PromotedAttraction_igroup4 SET tripProfile = '1,2' WHERE attracionID = 'Kalverstraat'
+	select COUNT(*) as count from PromotedAttraction_igroup4 where agent_ID=1 AND attracionID='Kalverstraat'
+	INSERT INTO PromotedAttraction_igroup4 (attracionID,rate,cityName)Values('W__145953534','1','Amsterdam')
+
+	INSERT INTO PromotedAttraction_igroup4 (attracionID,rate,cityName,agent_ID)Values('W__44508814','3','Amsterdam','1')
