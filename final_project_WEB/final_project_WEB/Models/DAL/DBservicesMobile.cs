9516998731e207efd3abe575c97f1b2d1f5eec1a
Ori Hayun <59655711+OriHayun@users.noreply.github.com>
2020-05-09 14:11:49 +0300

    Update DbsMobileServices

--- final_project_WEB/final_project_WEB/Models/DAL/DBservicesMobile.cs
+++ final_project_WEB/final_project_WEB/Models/DAL/DBservicesMobile.cs
@@ -586,8 +586,17 @@ public class DBservicesMobile
         try
         {
             con = connect("DBConnectionString"); // create a connection to the database using the connection String defined in the web config file
+            String selectSTR = "";
+            if (tripProfile==1)
+            {
+                selectSTR = "select * from PromotedAttraction_igroup4 where agent_ID='" + agentId + "'and cityName ='" + city + "'order by rate DESC";
 
-            String selectSTR = "select * from PromotedAttraction_igroup4 where agent_ID='" + agentId + "'and cityName ='" + city + "' and _" + tripProfile + "=1 order by rate DESC";
+            }
+            else
+            {
+                selectSTR = "select * from PromotedAttraction_igroup4 where agent_ID='" + agentId + "'and cityName ='" + city + "' and _" + tripProfile + "=1 order by rate DESC";
+
+            }
             SqlCommand cmd = new SqlCommand(selectSTR, con);
 
             // get a reader
@@ -598,13 +607,6 @@ public class DBservicesMobile
                 Promotion p = new Promotion();
 
                 p.AttractionID = (string)dr["attractionID"];
-                p.Rate = Convert.ToInt16(dr["rate"]);
-                p.TripProfile_2 = Convert.ToInt16(dr["_2"]);
-                p.TripProfile_3 = Convert.ToInt16(dr["_3"]);
-                p.TripProfile_4 = Convert.ToInt16(dr["_4"]);
-                p.TripProfile_5 = Convert.ToInt16(dr["_5"]);
-                p.TripProfile_6 = Convert.ToInt16(dr["_6"]);
-
                 PromotionAttractions.Add(p);
             }
             return PromotionAttractions;
