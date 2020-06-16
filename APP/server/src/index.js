9725b8439aa9bb365794df1d0477c99fc40a25f4
Ori Hayun <59655711+OriHayun@users.noreply.github.com>
2020-06-16 12:24:50 +0300

    add server

---
+++ APP/server/src/index.js
@@ -0,0 +1,38 @@
+require('./models/Users');
+
+const express = require('express');
+const mongoos = require('mongoose');
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(authRoutes);
+
+
+const mongoUri = 'mongodb+srv://hayunori:Oh04081993@cluster0-txjme.azure.mongodb.net/test?retryWrites=true&w=majority';
+mongoos.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+mongoos.connection.on('connected', () => {
+    console.log('connected to mongo instance ');
+});
+
+mongoos.connection.on('error', (err) => {
+    console.error('Error conneectio mongo ', err);
+});
+
+const port = process.env.PORT || 8080;
+
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(port, () => {
+    console.log('Listening to port'+port)
+});
